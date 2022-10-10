418b275fa0c3fad0a83cc14dd425dbbaac5885d2
lurk101 <lurkone0one@gmail.com>
2022-10-10 19:24:08 -0400

    restore missing instruction at end of function disassembly (cosmetic)

--- src/cc.c
+++ src/cc.c
@@ -3577,7 +3577,7 @@ static void stmt(int ctx) {
                     printf("%d: %.*s\n", lineno, p - lp, lp);
                     lp = p;
                     disasm_address(&state, (int)(se + 1));
-                    while (state.address < (int)e - 2) {
+                    while (state.address < (int)e - 1) {
                         uint16_t* nxt = (uint16_t*)(state.address + state.size);
                         disasm_thumb(&state, *nxt, *(nxt + 1));
                         printf("%s\n", state.text);
