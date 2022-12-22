24d2c3a0281a77534307985e260bae49b249323d
lurk101 <lurkone0one@gmail.com>
2022-12-21 20:04:55 -0500

    convert dos to unix format

--- sdio/hw_config.h
+++ sdio/hw_config.h
@@ -18,21 +18,8 @@ specific language governing permissions and limitations under the License.
 */
 #pragma once
 
-#include "ff.h"
 #include "sd_card.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-size_t sd_get_num();
-sd_card_t* sd_get_by_num(size_t num);
 
 size_t spi_get_num();
 spi_t* spi_get_by_num(size_t num);
 
-#ifdef __cplusplus
-}
-#endif
-
-/* [] END OF FILE */
